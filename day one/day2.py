cad60818353b2e06e97393865962a03d7da183ea
pmc-muchiri <pmc.ac.ke@gmail.com>
2022-07-28 06:33:26 +0300

    Day 2 of 100 dayts of code in python

---
+++ day one/day2.py
@@ -0,0 +1,25 @@
+#Data type
+
+#String
+#Subscripting
+print("Hello"[4])
+
+#concatinate
+a = "1234" + "5678"
+print(a)
+
+#integer
+b = 1234 + 5678
+print(b)
+
+#Float
+c = 3.14159 +2.345
+print(c)
+
+#Boolean
+
+True
+False
+
+#------------------------------------------#
+#quiz
