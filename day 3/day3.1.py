ec4e936617a76c2bdddae7c09262bd8620374491
pmc-muchiri <pmc.ac.ke@gmail.com>
2022-07-29 06:29:52 +0300

    if else condition

--- day 3/day3.1.py
+++ day 3/day3.1.py
@@ -1,1 +1,10 @@
 #control flow with if/ else and conditinal operations
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+if number%2 == 0:
+  print(f"{number} is even because {number} is divisible by 2")
+else:
+  print(f"{number} is odd because {number} is not divisible by 2")
+  
+#Write your code below this line 👇
