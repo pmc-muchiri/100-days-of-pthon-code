65cba543b0ddd95edbe1c6172dc27269840ef418
pmc-muchiri <pmc.ac.ke@gmail.com>
2022-07-29 06:12:24 +0300

    will finish thi bill calc

---
+++ day one/day2.5.py
@@ -0,0 +1,5 @@
+Print("Welcome to The tip Calculator! ")
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12 or 15? "))
+people = int(input("How many people to split the bill? "))
+
